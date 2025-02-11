e8c0e271e1b3ae2505a1530de9334f4786272b3a
Acreus <118129344+Acreus@users.noreply.github.com>
2025-02-12 01:19:26 +0530

    Update form handler.php

--- form handler.php
+++ form handler.php
@@ -2,14 +2,14 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$email_from = 'https://8fca0086-4fd8-4253-a868-f40e63bed600-00-25yx0uf340kar.pike.replit.dev/index.html';
+$email_from = 'https://acreus.github.io/Anindya-Portfolio-website/';
 $email_subject = "New Form Submission";
 $email_body = "user name: $name.\n".
               "user email: $email.\n".
               "user message: $message.\n".
-$to = "buildneo.23@gmail.com";
+$to = "mukherjeeanindya6918@gmail.com";
 $headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $email \r\n";
 mail($to,$email_subject,$email_body,$headers);
-header("Location: contact.html");
+header("Location: index.html");
 ?>
